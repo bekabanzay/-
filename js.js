cd0098083e4919898f070b16cb6a2df21f76becd
bekabanzay <103204376+bekabanzay@users.noreply.github.com>
2022-04-07 20:32:46 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+function greet(firstname, lastname) {
+	console.log('assalomu aleykum, ' + firstname + ' ' + lastname);
+}
+
+function square(number) {
+	return number * number;
+}
+
+greet('ahmad', 'abdullayev');
+greet('hamid');
+console.log(square(2));
